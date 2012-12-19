f95cd1059a32b995e9934180cb380e04073df038
Tom Marshall <tdm.code@gmail.com>
2012-12-19 10:00:28 -0800

    Initial commit.

---
+++ aokp.mk
@@ -0,0 +1,19 @@
+# Release name
+PRODUCT_RELEASE_NAME := Iproj
+
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/iproj/full_iproj.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := iproj
+PRODUCT_NAME := aokp_iproj
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := LG-IPROJ
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_CHARACTERISTICS := phone
---
+++ aokp_iproj.mk
@@ -0,0 +1,3 @@
+# Bridge for AOSP/AOKP, see AndroidProducts.mk
+include device/lge/iproj/full_iproj.mk
+include device/lge/iproj/aokp.mk
